c81d898f59d0a17d7aa072897b73ddd16bdb71bf
Martin Printz <martin.printz@evosoft.com>
2023-09-11 11:42:15 +0200

    add: example

---
+++ ix-tooltip-error/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ix-tooltip-error';
+  count = ['first', 'second']
+}
